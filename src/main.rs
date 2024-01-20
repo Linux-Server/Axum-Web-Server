f9a75576f9b2c35afbf9d37e3febe275671deb88
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-21 03:20:45 +0530

    any how : ensure!() + anyhow!()

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
 use std::fs::{File};
 use std::io::Error;
-use anyhow::Result;
+use anyhow::{anyhow, Context, ensure, Result};
+use anyhow::__private::kind::{AdhocKind, TraitKind};
+
 fn main() {
     println!("Hello, world!");
     let x = check(true);
-    println!("The out is {:?}", x);
+    println!("{:#?}", x);
+
+    match x{
+        Err(val) => {
+             println!("The vals : {:?}", val);
+        },
+        _ => ()
+    }
+
+    let z = check_two(false);
+    println!("The second is {:?}", z);
 }
 
 
 fn check(val:bool)-> Result<()>{
-   let file_open = File::open("hello.txt")?;
+    // ensure!(false, "Not true");
+    let file_open = File::open("hello.txt").with_context(|| format!("NEVER MISS THE GAME"))?;
     println!("the stat is {:?}", file_open);
     Ok(())
+}
+
+
+fn check_two(val:bool)-> Result<()>{
+    match val {
+        true => Ok(()),
+        false => Err(anyhow!("Not applicable"))
+    }
 }
 
 //The out is Err(Os { code: 2, kind: NotFound, message: "No such file or directory" })
