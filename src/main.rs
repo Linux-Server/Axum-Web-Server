f6a2bcb1a8df93834e7a31daa60d479205f3d8e3
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-21 04:34:13 +0530

    axum with reqwest http client crate added

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,14 @@
-use std::fs::{File};
-use std::io::Error;
-use anyhow::{anyhow, Context, ensure, Result};
-use anyhow::__private::kind::{AdhocKind, TraitKind};
+#![allow(unused)]
+use anyhow::{Result};
+#[tokio::main]
+async fn main()-> Result<()> {
+    println!("The axum web-server");
 
-fn main() {
-    println!("Hello, world!");
-    let x = check(true);
-    println!("{:#?}", x);
+    let body = reqwest::get("https://jsonplaceholder.typicode.com/todos/1")
+        .await?
+        .text()
+        .await?;
 
-    match x{
-        Err(val) => {
-             println!("The vals : {:?}", val);
-        },
-        _ => ()
-    }
-
-    let z = check_two(false);
-    println!("The second is {:?}", z);
-}
-
-
-fn check(val:bool)-> Result<()>{
-    // ensure!(false, "Not true");
-    let file_open = File::open("hello.txt").with_context(|| format!("NEVER MISS THE GAME"))?;
-    println!("the stat is {:?}", file_open);
+    println!("body = {}", body);
     Ok(())
 }
-
-
-fn check_two(val:bool)-> Result<()>{
-    match val {
-        true => Ok(()),
-        false => Err(anyhow!("Not applicable"))
-    }
-}
-
-//The out is Err(Os { code: 2, kind: NotFound, message: "No such file or directory" })
